f1c894d61b7d340709ad2d205ebdfbe484c2a026
W3LabKr <w3labkr@gmail.com>
2025-02-05 19:32:34 +0900

    Edit auth error style

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react'
 import type { Metadata } from 'next'
-import Link from 'next/link'
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
+import { AlertCircle } from 'lucide-react'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 
 export const metadata: Metadata = {
   title: 'Auth Error',
@@ -20,19 +19,13 @@ export default function AuthErrorPage({
   const error = searchParams?.error?.toString() || 'unknown'
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-2xl">Something went wrong</CardTitle>
-      </CardHeader>
-      <CardContent>
+    <Alert>
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>Something went wrong</AlertTitle>
+      <AlertDescription>
         There was a problem when trying to authenticate. Please contact us if this error persists. Unique error code:{' '}
         <code className="rounded-sm bg-slate-100 p-1 text-xs">{error}</code>
-        <br />
-        <br />
-        <Link href="/auth/login" className="underline underline-offset-4">
-          sign in
-        </Link>
-      </CardContent>
-    </Card>
+      </AlertDescription>
+    </Alert>
   )
 }
