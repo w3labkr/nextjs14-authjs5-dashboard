633d274fc9ea8164d88cb1add6ee3361ba3e4b15
W3LabKr <w3labkr@gmail.com>
2025-02-15 03:40:12 +0900

    chore: remove unnecessary code

--- lib/dayjs.ts
+++ lib/dayjs.ts
@@ -5,19 +5,11 @@ import timezone from 'dayjs/plugin/timezone'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
 
 import 'dayjs/locale/ko'
-import 'dayjs/locale/ja'
-import 'dayjs/locale/zh-cn'
-import 'dayjs/locale/zh-tw'
-import 'dayjs/locale/zh-hk'
-import 'dayjs/locale/zh'
 
 extend(utc)
 extend(timezone)
 extend(localizedFormat)
 
-const timestamp = Date.now()
-const tz = 'Asia/Seoul'
+dayjs.tz.setDefault('Asia/Seoul')
 
-dayjs.tz.setDefault(tz)
-
-export default dayjs
+export default dayjs.tz
