ba8c50c2fba036ec07e58e308130e5fb2e485566
W3LabKr <w3labkr@gmail.com>
2025-02-15 15:14:57 +0900

    chore: add reactStrictMode setting to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Suggested: We strongly suggest you enable Strict Mode in your Next.js application to better prepare your application for the future of React.
+  reactStrictMode: process.env.NODE_ENV === 'production',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
